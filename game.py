f8360f9da6d3d55aec148653e98bbc05f7882adc
Argon-cell <n4fikov.ilgam@gmail.com>
2020-07-15 22:09:45 +0300

    ds
    asda
    deferlgbr,stb dFGkg3ends
    lease enter the commit message for your changes. Lines starting

--- game.py
+++ game.py
@@ -17,11 +17,33 @@ class Map(object):
         pass
 
 
+def makeMatrix(width, rows, window):
+    sizeBtwn = width // rows
+    x = 0
+    y = 0
+    for i in range(rows):
+        x += sizeBtwn
+        y += sizeBtwn
+        pygame.draw.line(window, (255, 255, 255), (x, 0), (x, width))
+        pygame.draw.line(window, (255, 255, 255), (0, y), (x, width))
+
+
+def drawMatrix(display):
+    display.fill((0,0,0))
+    makeMatrix(width, rows, display)
+    pygame.display.update()
+
+
 def main():
     width = 500
     height = 500
-    x, y = randrange(0, width), randrange(0, width)
-    length = 1
-    snake = Snake((x, y), length)
+    rows = 20 #number of rows
     window = pygame.display.set_mode((width, height))
     pygame.display.set_caption("Snake the game")
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        pygame.time.delay(50)
+        clock.tick(10)
+        drawMatrix(window)
+main()
