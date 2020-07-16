b539c23c294c6ad27fa11df0d9c936ff1166a061
Argon-cell <n4fikov.ilgam@gmail.com>
2020-07-16 18:03:15 +0300

    updates

--- setting.py
+++ setting.py
@@ -1,2 +1,3 @@
 # There is setting for game
-RESOLUTION=(WIDTH, HEIGHT)=(600,600)
+RESOLUTION = (WIDTH, HEIGHT) = (600, 600)
+ROWS = 20  # number of horizontal / vertical lines
--- window.py
+++ window.py
@@ -8,19 +8,19 @@ class Window:
         pg.display.set_caption("SNAKE")
         ico = pg.image.load("snake.png")
         pg.display.set_icon(ico)
+        self.window = pg.display.set_mode((WIDTH, HEIGHT))
         self.screen = pg.display.set_mode((setting.WIDTH, setting.HEIGHT))
+        self.distanceBetween = WIDTH // ROWS
 
     def create_matrix(self):
         x = 0
         y = 0
-        for i in range(rows):
-            x += lengBtwn
-            y += lengBtwn
-            pygame.draw.line(window, (255, 255, 255), (x, 0), (x, HEIGHT))
-            pygame.draw.line(window, (255, 255, 255), (0, y), (WIDTH, y))
-        
+        for i in range(ROWS):
+            x += self.distanceBetween
+            y += self.distanceBetween
+            pg.draw.line(self.window, (255, 255, 255), (x, 0), (x, HEIGHT))
+            pg.draw.line(self.window, (255, 255, 255), (0, y), (WIDTH, y))
 
     def update(self):
         self.create_matrix()
         # TODO
-
