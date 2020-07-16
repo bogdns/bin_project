a53de8823e665f09c0d54e04df43debcac764c36
Argon-cell <n4fikov.ilgam@gmail.com>
2020-07-16 23:21:56 +0300

    food copy

--- food.py
+++ food.py
@@ -1,13 +1,33 @@
-import pygame
 from random import randrange
-from snake import *
-class Food:
-    def __init__(self, width):
-        self.foodOnMap = 0
-        self.coordfoodX = randrange(0, width)
-        self.coordfoodY = randrange(0, width)
+from setting import *
+import pygame as pg
 
-    def food_eated(self):
-        if S == self.coordfoodX and y_coord == self.coordfoodY:
-            length += 1
-            self.foodOnMap = 0
+
+class Food:
+    def __init__(self, window):
+        self.pos = (0, 0)  # init pos of a food
+        self.distanceBetween = WIDTH // ROWS
+        self.screen = window
+
+    def calculate_pos(self, snake_pos):
+        pass
+        """
+        calculates position of a food
+        """
+
+        return cords  # return position. cortege
+
+    def draw_food(self, snake_pos):
+        """
+        spawns like cells of the snake
+        """
+        pg.draw.rect(self.screen, (0, 255, 0),
+                     (self.calculate_pos(snake_pos), self.distanceBetween, self.distanceBetween))
+
+    def update(self, snake_pos):
+        """
+        spawns food with library randint
+        gets deque with positions of the snake and returns pos of the food
+        """
+        self.draw_food(snake_pos)
+        # TODO
