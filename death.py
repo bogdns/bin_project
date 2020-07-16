93e690a7f15cd9593023b258b756bb8ea13aa2ed
Argon-cell <n4fikov.ilgam@gmail.com>
2020-07-17 00:29:24 +0300

    big changes; added death without field

--- death.py
+++ death.py
@@ -6,7 +6,7 @@ class Death:
     def __init__(self, pos, window, run):
         self.pos = pos
         self.window = window
-        self.font = pg.font.SysFont('comicsansms', 32)
+        # self.font = pg.font.SysFont('comicsansms', 32)
         self.death = False
 
     def field_check(self):
@@ -14,5 +14,5 @@ class Death:
         headY = self.pos[-1][1]
         if (headX > ROWS or headX < 0) or (headY > ROWS or headY < 0):
             self.window.fill((0, 0, 0))
-            self.font.render("Game over", 1, (0, 255, 0), (0, 0, 0))
+            # self.font.render("Game over", 1, (255, 255, 255), (255, 255, 255))
             self.death = True
