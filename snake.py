1c91d9d0303504170a601dda444d8951248f75ad
Argon-cell <n4fikov.ilgam@gmail.com>
2020-07-16 22:26:47 +0300

    change pixels on rect

--- snake.py
+++ snake.py
@@ -14,16 +14,17 @@ class Snake:
         self.directionY = 0
         self.pos = []  # positions of the snake(what cells is it on)
         self.distanceBetween = WIDTH // ROWS
-        self.center = (ROWS // 2) * self.distanceBetween
-        self.pos.append((self.center, self.center))
-        self.pos.append((self.center + self.distanceBetween, self.center + self.distanceBetween))# places head of the snake in the center
+        self.pos.append((ROWS // 2, ROWS // 2))  # places head of the snake in the center
+        self.pos.append((ROWS // 2 + 1, ROWS // 2 + 1))  # places body of the snake in the center
 
     def draw_green_cell(self, cords):
         pg.draw.rect(self.window, (0, 255, 0), (*cords, self.distanceBetween, self.distanceBetween))
 
     def draw_snake(self):
         for cords in self.pos:
-            self.draw_green_cell(cords)
+            x = cords[0]
+            y = cords[1]
+            self.draw_green_cell((x * self.distanceBetween, y * self.distanceBetween))
 
     def update(self):
         key = pg.key.get_pressed()
@@ -40,6 +41,6 @@ class Snake:
             self.directionY = 1
             self.directionX = 0
 
-        self.pos.append((self.pos[-1][0] + self.directionX * self.distanceBetween,
-                         self.pos[-1][1] + self.directionY * self.distanceBetween))
+        self.pos.append((self.pos[-1][0] + self.directionX,
+                         self.pos[-1][1] + self.directionY))
         self.pos = self.pos[self.length:]
