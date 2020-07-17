d97a46dcfca39c25c6e6e1ffb763948f87da039e
Bogdan <salimovb2002@mail.ru>
2020-07-17 14:09:54 +0300

    fix

--- snake.py
+++ snake.py
@@ -6,8 +6,8 @@ import pygame as pg
 class Snake:
     def __init__(self, window):
         self.window = window
-        self.directionX = 0
-        self.directionY = -1
+        self.direction_x = 0
+        self.direction_y = -1
         self.pos = deque()  # positions of the snake(what cells is it on)
         self.distanceBetween = WIDTH // ROWS
         self.pos.append((ROWS // 2, ROWS // 2))  # places head of the snake in the center
@@ -21,22 +21,22 @@ class Snake:
 
     def update(self, ate):
         key = pg.key.get_pressed()
-        if key[pg.K_RIGHT]:
-            self.directionX = 1
-            self.directionY = 0
-        if key[pg.K_LEFT]:
-            self.directionX = -1
-            self.directionY = 0
-        if key[pg.K_UP]:
-            self.directionX = 0
-            self.directionY = -1
-        if key[pg.K_DOWN]:
-            self.directionX = 0
-            self.directionY = 1
+        if key[pg.K_RIGHT] and self.direction_x != -1:
+            self.direction_x = 1
+            self.direction_y = 0
+        if key[pg.K_LEFT] and self.direction_x != 1:
+            self.direction_x = -1
+            self.direction_y = 0
+        if key[pg.K_UP] and self.direction_y != 1:
+            self.direction_x = 0
+            self.direction_y = -1
+        if key[pg.K_DOWN] and self.direction_y != -1:
+            self.direction_x = 0
+            self.direction_y = 1
         if ate:
             self.pos.appendleft(self.tail)
-        self.pos.append((self.pos[-1][0] + self.directionX,
-                         self.pos[-1][1] + self.directionY))
+        self.pos.append((self.pos[-1][0] + self.direction_x,
+                         self.pos[-1][1] + self.direction_y))
         self.tail = self.pos.popleft()
 
         for cords in self.pos:
