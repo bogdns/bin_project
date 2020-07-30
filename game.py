15f5820f30b3471855dd424f03c5a0c74214a3fc
Bogdan <salimovb2002@mail.ru>
2020-07-30 04:37:01 +0300

    clear

--- game.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-
-pg.init()
-
-from window import *
-
-window = Window()
-window.update()
--- quit_game.py
+++
@@ -1,5 +0,0 @@
-import pygame as pg
-def quit():
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            exit(0)
